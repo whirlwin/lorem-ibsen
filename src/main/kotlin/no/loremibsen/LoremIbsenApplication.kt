ccd032e7c15ff4b5a82e24741e2ee3d1a1b779c2
Øyvind <oyvind.odegaard@dfind.no>
2018-08-03 14:52:21 +0200

    Inital commit

---
+++ src/main/kotlin/no/loremibsen/LoremIbsenApplication.kt
@@ -0,0 +1,11 @@
+package no.loremibsen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoremIbsenApplication
+
+fun main(args: Array<String>) {
+    runApplication<LoremIbsenApplication>(*args)
+}
